2a2169e22c621beb386eb021e58b3db96d1415eb
omosehin <omosehin14gmail.com>
2018-11-28 23:35:28 +0100

    one

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+//import Aux from "../../hoc/Aux";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <div>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </div>
+    );
+  }
+}
+
+export default BurgerBuilder;
